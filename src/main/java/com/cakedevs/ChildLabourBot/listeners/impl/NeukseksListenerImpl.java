102541ef342edbc57d26e0a08fdb5e15ac0a413f
Benjamin Veldhuizen <benjaminveld02@gmail.com>
2021-12-21 23:22:41 +0100

    adds delay timer

--- src/main/java/com/cakedevs/ChildLabourBot/listeners/impl/NeukseksListenerImpl.java
+++ src/main/java/com/cakedevs/ChildLabourBot/listeners/impl/NeukseksListenerImpl.java
@@ -9,6 +9,9 @@ import org.javacord.api.event.message.MessageCreateEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
@@ -23,68 +26,81 @@ public class NeukseksListenerImpl implements NeukseksListener {
     @Autowired
     private UserRepository userRepository;
 
+    private HashMap<String, LocalDateTime> cooldowns = new HashMap<String, LocalDateTime>();
+
     @Override
     public void onMessageCreate(MessageCreateEvent messageCreateEvent) {
         AtomicBoolean done = new AtomicBoolean(false);
 
-        if(messageCreateEvent.getMessageContent().startsWith("+neukseks")) {
-            String[] command = messageCreateEvent.getMessageContent().split(" ");
-            if (command.length > 1) {
-                String userID = command[1];
-                userID = userID.replace("<", "");
-                userID = userID.replace("@", "");
-                userID = userID.replace("!", "");
-                userID = userID.replace(">", "");
-                Optional<User> userOpt = userRepository.findUserById(userID);
+        if (cooldowns.containsKey(messageCreateEvent.getMessageAuthor().getIdAsString())) {
+            if (cooldowns.get(messageCreateEvent.getMessageAuthor().getIdAsString()).isBefore(LocalDateTime.now())) {
+                if(messageCreateEvent.getMessageContent().startsWith("+neukseks")) {
+                    String[] command = messageCreateEvent.getMessageContent().split(" ");
+                    if (command.length > 1) {
+                        String userID = command[1];
+                        userID = userID.replace("<", "");
+                        userID = userID.replace("@", "");
+                        userID = userID.replace("!", "");
+                        userID = userID.replace(">", "");
+                        Optional<User> userOpt = userRepository.findUserById(userID);
 
-                if (userOpt.isPresent()) {
-                    if (messageCreateEvent.getMessageAuthor().getId() != Long.parseLong(userOpt.get().getId())) {
-                        Random r = new Random();
-                        int num1 = r.nextInt(20);
-                        int num2 = r.nextInt(100);
+                        if (userOpt.isPresent()) {
+                            if (messageCreateEvent.getMessageAuthor().getId() != Long.parseLong(userOpt.get().getId())) {
+                                Random r = new Random();
+                                int num1 = r.nextInt(20);
+                                int num2 = r.nextInt(100);
 
-                        try {
-                            String finalUserID = userID;
-                            messagingService.sendMessage(messageCreateEvent.getMessageAuthor(),
-                                     messageCreateEvent.getApi().getUserById(userID).get().getName() + " wil je kontjebonken met "
-                                             + messageCreateEvent.getApi().getUserById(messageCreateEvent.getMessageAuthor().getId()).get().getName() + "?",
-                                    "Degene die als eerst de rekensom oplost, raakt zwanger.",
-                                    null,
-                                    null,
-                                    messageCreateEvent.getChannel())
-                            .thenAccept(message -> {
-                                message.addReaction("\uD83D\uDC4D");
-                                message.addReaction("\uD83D\uDC4E");
-                                message.addReactionAddListener(listener -> {
-                                    if (listener.getEmoji().equalsEmoji("\uD83D\uDC4D") && listener.getUser().get().getId() == Long.parseLong(finalUserID)) {
-                                        message.edit(new EmbedBuilder()
-                                                .setTitle("Lekkere neukseks hmmm")
-                                                .setDescription(num1 + " * " + num2)
-                                                .setFooter("ziek man"));
-                                        message.getChannel().addMessageCreateListener(messageCreateListener -> {
-                                            if (messageCreateListener.getMessageContent().equals(Integer.toString(num1*num2)) && !done.get()) {
-                                                messageCreateEvent.getChannel().sendMessage(messageCreateListener.getMessageAuthor().getName() + " took the kids. Can I at least see them at Christmas?");
-                                                done.set(true);
+                                try {
+                                    String finalUserID = userID;
+                                    messagingService.sendMessage(messageCreateEvent.getMessageAuthor(),
+                                             messageCreateEvent.getApi().getUserById(userID).get().getName() + " wil je kontjebonken met "
+                                                     + messageCreateEvent.getApi().getUserById(messageCreateEvent.getMessageAuthor().getId()).get().getName() + "?",
+                                            "Degene die als eerst de rekensom oplost, raakt zwanger.",
+                                            null,
+                                            null,
+                                            messageCreateEvent.getChannel())
+                                    .thenAccept(message -> {
+                                        message.addReaction("\uD83D\uDC4D");
+                                        message.addReaction("\uD83D\uDC4E");
+                                        message.addReactionAddListener(listener -> {
+                                            if (listener.getEmoji().equalsEmoji("\uD83D\uDC4D") && listener.getUser().get().getId() == Long.parseLong(finalUserID)) {
+                                                message.edit(new EmbedBuilder()
+                                                        .setTitle("Lekkere neukseks hmmm")
+                                                        .setDescription(num1 + " * " + num2)
+                                                        .setFooter("ziek man"));
+                                                message.getChannel().addMessageCreateListener(messageCreateListener -> {
+                                                    if (messageCreateListener.getMessageContent().equals(Integer.toString(num1*num2)) && !done.get()) {
+                                                        messageCreateEvent.getChannel().sendMessage(messageCreateListener.getMessageAuthor().getName() + " took the kids. Can I at least see them at Christmas?");
+                                                        done.set(true);
+                                                    }
+                                                    LocalDateTime now = LocalDateTime.now();
+                                                    LocalDateTime cooldown = now.plusHours(4);
+                                                    cooldowns.put(messageCreateEvent.getMessageAuthor().getIdAsString(), cooldown);
+                                                });
+                                            } else if (listener.getEmoji().equalsEmoji("\uD83D\uDC4E") && listener.getUser().get().getId() == Long.parseLong(finalUserID)) {
+                                                message.edit(new EmbedBuilder()
+                                                        .setTitle("Jammer dan")
+                                                        .setDescription("Geen neukseks for you."));
                                             }
                                         });
-                                    } else if (listener.getEmoji().equalsEmoji("\uD83D\uDC4E") && listener.getUser().get().getId() == Long.parseLong(finalUserID)) {
-                                        message.edit(new EmbedBuilder()
-                                                .setTitle("Jammer dan")
-                                                .setDescription("Geen neukseks for you."));
-                                    }
-                                });
-                            });
-                        } catch (InterruptedException | ExecutionException e) {
-                            e.printStackTrace();
+                                    });
+                                } catch (InterruptedException | ExecutionException e) {
+                                    e.printStackTrace();
+                                }
+                            } else {
+                                messageCreateEvent.getChannel().sendMessage("Bro wtf fak? je kan dit letterlijk niet op jezelf doen.");
+                            }
+                        } else {
+                            messageCreateEvent.getChannel().sendMessage("Deze meneer heeft geen ChildLabourSimulator account.");
                         }
                     } else {
-                        messageCreateEvent.getChannel().sendMessage("Bro wtf fak? je kan dit letterlijk niet op jezelf doen.");
+                        messageCreateEvent.getChannel().sendMessage("Bro ga iemand pingen in je command dan ofzo?");
                     }
-                } else {
-                    messageCreateEvent.getChannel().sendMessage("Deze meneer heeft geen ChildLabourSimulator account.");
                 }
             } else {
-                messageCreateEvent.getChannel().sendMessage("Bro ga iemand pingen in je command dan ofzo?");
+                Duration difference = Duration.between(LocalDateTime.now(), cooldowns.get(messageCreateEvent.getMessageAuthor().getIdAsString()));
+                messageCreateEvent.getChannel().sendMessage("Bro rustig man bro, je moet nog " + difference.toHours() + " uur, "
+                        + difference.toMinutesPart() + " minuten en " + difference.toSecondsPart() + " seconden wachten.");
             }
         }
     }
