6c8ec43942a553496ea7985099a231a9a9e1d3f5
Benjamin Veldhuizen <benjaminveld02@gmail.com>
2021-12-22 14:35:39 +0100

    fix make child

--- src/main/java/com/cakedevs/ChildLabourBot/repository/ChildRepository.java
+++ src/main/java/com/cakedevs/ChildLabourBot/repository/ChildRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface ChildRepository extends CrudRepository<Child, Long> {
     Optional<Child> findChildById(long id);
-    List<Child> findChildsByUser_id(String user_id);
+    List<Child> findChildsByUserid(String userid);
 }
