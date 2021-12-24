00e28e2e6e2b23e67a77bdb06c6f260f3b2ef729
Benjamin Veldhuizen <benjaminveld02@gmail.com>
2021-12-24 17:28:46 +0100

    bug fixes

--- src/main/java/com/cakedevs/ChildLabourBot/listeners/impl/NeukseksListenerImpl.java
+++ src/main/java/com/cakedevs/ChildLabourBot/listeners/impl/NeukseksListenerImpl.java
@@ -100,7 +100,7 @@ public class NeukseksListenerImpl implements NeukseksListener {
                                                         .setDescription(num1 + " * " + num2)
                                                         .setFooter("ziek man"));
                                                 message.getChannel().addMessageCreateListener(answerListener -> {
-                                                    if (answerListener.getMessageContent().equals(Integer.toString(num1 * num2)) && !done.get()) {
+                                                    if (answerListener.getMessageContent().equals(Integer.toString(num1 * num2)) && !done.get() && answerListener.getMessageAuthor().getIdAsString() == finalUserID) {
                                                         cooldown.get().setNeuksekscooldown(System.nanoTime() + (delayInMinutes * 60000000000L));
                                                         cooldownRepository.save(cooldown.get());
                                                         done.set(true);
